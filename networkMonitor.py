12cbb1c76aa21371eb6cf029702b5f772f102254
nkeels <nikeels92@gmail.com>
2017-10-05 16:48:33 -0600

    Update networkMonitor.py

--- networkMonitor.py
+++ networkMonitor.py
@@ -21,13 +21,19 @@ def Email(content):
     mail.close 
 
 ip = ['10.25.128.225', '10.25.128.223', '10.25.128.224', '10.25.128.241']
+err = []
 
 for address in ip:
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     result = sock.connect_ex((address, 9999))
-    if result != 0: 
-        Email('There is an issue with the {} address'.format(address))
+    if result != 0:
+        err.append(address)
 
+
+if err != None:
+    Email("There is an error with the {} address(es)! Please check as soon as possible!!".format(err))
+else:
+    quit()
 
 
 
