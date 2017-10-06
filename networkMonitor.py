449a0644e5747d4ffa834cb789c439c3c4c4ab1e
nkeels <nikeels92@gmail.com>
2017-10-06 15:46:27 -0600

    Update networkMonitor.py

--- networkMonitor.py
+++ networkMonitor.py
@@ -7,32 +7,24 @@
 #                                           -Nicholas Keels 2017#
 #***************************************************************#
 
-import smtplib
-import socket
+mport socket
+import contact_lib
 
-def Email(content):
-    content
-    mail = smtplib.SMTP('smtp.gmail.com',587)
-    mail.ehlo()
-    mail.starttls()
-    mail.login('emailaddress@fqdm.com','password')
-    mail.sendmail('emailaddress@fqdm.com', 'recievingemail@whatever.com', content)
-    mail.close 
 
 ip = ['10.25.128.225', '10.25.128.223', '10.25.128.224', '10.25.128.241']
 err = []
+
 
 for address in ip:
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     result = sock.connect_ex((address, 9999))
     if result != 0:
         err.append(address)
-        
-if err != None:
-    Email("There is an error with the {} address(es)! Please check as soon as possible!!".format(err))
-else:
-    quit()
 
+if err != None:
+    print(err)
+else:
+    print(err)
 
 
 
