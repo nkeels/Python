58ff2b360469ee4f28d3a179b48abb28edf4d6bc
nkeels <nikeels92@gmail.com>
2018-03-06 11:55:06 -0700

    THis is the first attempt at a script

--- ipAddCheck.py
+++ ipAddCheck.py
@@ -1,5 +1,11 @@
 import os
 
-bashCommand = "sudo arp-scan --interface=enp0s25 --localnet"
 
+bashCommand = "sudo arp-scan --interface=enp0s25 --localnet | grep 192.168.2.* > output.txt"
 os.system(bashCommand)
+
+f = open('output.txt', 'r')
+print f.read()
+
+print f
+
